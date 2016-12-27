38ac614961bfb9e43e7d482aaa2855eb8d1c077a
automatwon <automatwon@gmail.com>
2016-12-26 18:09:46 -0800

    Add gulp task to generate p5 ts definitions

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,15 @@
 const gulp = require('gulp');
+const p5dtsgenerator = require('./scripts/generate-p5-typescript-definition');
+const paths = {
+    generatedP5TDSTemp : 'scripts/generated',
+};
+
+// Convert p5dtsgenerator to return streams
+gulp.task('generate-p5-ts', () => {
+  return p5dtsgenerator.generate({
+      'outputDirName': paths.generatedP5TDSTemp
+  });
+});
 
 gulp.task('default', () => {
 
