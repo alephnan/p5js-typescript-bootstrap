5464047777ba1f4eb6e42bc0643a5fec1eaf875b
automatwon <automatwon@gmail.com>
2016-12-26 18:11:29 -0800

    Add gulp task to cleanup temp directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const del = require('del');
 const p5dtsgenerator = require('./scripts/generate-p5-typescript-definition');
 const paths = {
     generatedP5TDSTemp : 'scripts/generated',
@@ -13,6 +14,9 @@ gulp.task('generate-p5-ts', () => {
 gulp.task('move-p5-ts', ['generate-p5-ts'], () => {
   return gulp.src(paths.generatedP5TDSTemp + '/p5.d.ts')
     .pipe(gulp.dest('src'));
+});
+gulp.task('clean-generate-p5-ts', ['move-p5-ts'], () => {
+  return del(paths.generatedP5TDSTemp);
 });
 
 gulp.task('default', () => {
